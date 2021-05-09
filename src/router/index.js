e6ba571b2a8d7d4a22b4bfb7cac306215cc4b8f7
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-05-09 19:22:28 +0200

    setup

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-
 const routes = [
   {
     path: '/',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script> 
+<script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
