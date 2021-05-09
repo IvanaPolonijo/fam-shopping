286717d5a3b7dd2a20a0265a17b69ade2865a17a
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-05-09 19:20:37 +0200

    setup

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
@@ -15,5 +16,6 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
