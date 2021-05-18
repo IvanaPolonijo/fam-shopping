3d3295d3bd7058a3cfaf378d45d581402e77c442
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-05-18 16:24:24 +0200

    još malo interne

--- src/views/ShopList.vue
+++ src/views/ShopList.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="container">
     <div class="row">
-      <!-- <div class="col">first</div> -->
       <div class="col-8">
         <item-card />
         <item-card />
-        
       </div>
-      <div class="col-3">Search will come here</div>
+      <div class="col-3">
+        <div class="search">
+          <b-form-input
+            v-model="text"
+            placeholder="Find by tag"
+          ></b-form-input>
+          <div class="mt-2">Value za provjeru: {{ text }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,12 +21,23 @@
 <script>
 import ItemCard from "../components/item-card.vue";
 
-
 export default {
   name: "ShopList",
   components: {
     ItemCard,
- 
   },
+  data() {
+      return {
+        text: ''
+      }
+    }
 };
 </script>
+
+<style scoped>
+.search {
+  padding-top: 15px;
+ 
+
+}
+</style>
