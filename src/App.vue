ef08e6a66ad4557802daff6f5a212044d654b515
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-05-18 17:09:59 +0200

    internal pages tuning

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
   <div id="app">
     <div id="nav">
-      <b-row>
+      <div class="container">
+        <b-row >
+        <b-col cols="1"></b-col> 
         <b-col>
-          <b-navbar-brand align="left" tag="h1" class="mb-0">
+          <b-navbar-brand tag="h1" >
             FamPoloShopping
           </b-navbar-brand>
         </b-col>
         <b-col>
           <b-nav align="right">
             <b-button class="button" @click="$router.push('/')">Login</b-button>
-            <b-button class="button" @click="$router.push('about')">About</b-button>
+            <b-button @click="$router.push('about')">About</b-button>
           </b-nav>
         </b-col>
-      </b-row>
+       </b-row>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-
-
 #nav {
   padding: 30px;
   background: white;
-
 }
 
 #nav a {
@@ -37,9 +37,11 @@
   color: #42b983;
 }
 
-.button {
-  margin-right: 20px;
-
+.container {
+  max-width: 80%;
 }
 
+.button {
+  margin-right: 20px;
+}
 </style>
