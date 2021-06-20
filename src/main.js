ef4da7556dc9f242f541246dc0ccfaf7e3820603
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-06-20 15:19:35 +0200

    CSS ne loada - prvi dio

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import App from './App.vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
 
 // From https://vue-particles.netlify.app/
 import VueParticles from 'vue-particles'
@@ -14,9 +17,6 @@ import VueParticles from 'vue-particles'
 import VueSimpleAlert from "vue-simple-alert";
 
 Vue.use(VueSimpleAlert);
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
 
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
--- src/views/test.vue
+++ src/views/test.vue
@@ -37,4 +37,5 @@ export default {
 </script>
 
 <style>
+
 </style>
